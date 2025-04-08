8be8da9a3737551e4531c0bf3dbd4a1b9058cccd
hathanhphan <hathanh.0113@gmail.com>
2025-04-08 09:32:19 +0700

    update ss6

---
+++ ss6_javascript/thuc_hanh/index.js
@@ -0,0 +1,12 @@
+document.getElementById("btnChangeInnerHtml").onclick = function () {
+    document.getElementById("title").innerHTML = "Hello World!";
+}
+document.getElementById("btnDocWrite").onclick = function () {
+    document.write("Hello World!");
+}
+document.getElementById("btnAlert").onclick = function () {
+    alert("Hello World!");
+}
+document.getElementById("btnConsoleLog").onclick = function () {
+    console.log("Hello World!");
+}
